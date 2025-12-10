950752b9e804520db3518dea18f765c0def33687
credential.helper <android.touchofeden@gmail.com>
2025-12-10 18:08:47 +0700

    Powershell ...

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -288,6 +288,7 @@ function global:__zoxide_zi
 # Commands for zoxide. Disable these using --no-cmd.
 #
 
+#Z alias
 Set-Alias -Name z -Value __zoxide_z -Option AllScope -Scope Global -Force
 Set-Alias -Name zi -Value __zoxide_zi -Option AllScope -Scope Global -Force
 
